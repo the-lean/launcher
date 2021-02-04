1c1c0ab832053e3026f529b0d576b6c44d7982ad
Christoph Gattermayr <christoph.gattermayr@outlook.com>
2021-02-04 12:35:53 +0100

    Fixed remove widget bug + meanwhile disable remove dialog

--- src/app/src/main/java/leancher/android/ui/pages/Feed.kt
+++ src/app/src/main/java/leancher/android/ui/pages/Feed.kt
@@ -1,12 +1,17 @@
 package leancher.android.ui.pages
 
 import android.appwidget.AppWidgetHostView
+import android.content.pm.ProviderInfo
+import android.widget.LinearLayout
+import android.widget.ListPopupWindow.MATCH_PARENT
+import android.widget.ListPopupWindow.WRAP_CONTENT
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -48,7 +53,7 @@ fun Feed(vm: FeedViewModel) {
     Row {
         WidgetHostView(
             widgets = vm.widgets,
-            onRemoveWidget = vm::removeWidget,
+            removeWidget = vm::removeWidget,
             createWidgetHostView = vm.actions.createWidgetHostView
         )
     }
@@ -57,44 +62,44 @@ fun Feed(vm: FeedViewModel) {
 @Composable
 fun WidgetHostView(
     widgets: List<Widget>,
-    onRemoveWidget: (widget: Widget) -> Unit,
+    removeWidget: (widget: Widget) -> Unit,
     createWidgetHostView: (widget: Widget) -> AppWidgetHostView
 ) {
-    val context = AmbientContext.current
 
-    val (showDialog, setShowDialog) = remember { mutableStateOf(false) }
-    // https://developer.android.google.cn/reference/kotlin/androidx/compose/foundation/package-summary
-
-    // TODO: get dialog out of forEach loop (find suitable workaround for callback (with parameter)
-    // TODO: bugfix every time when a widget is deleted, the last one in the list is not rendered any more - why?
+    // TODO: show confirmation dialog before removing widget
+    //    val context = AmbientContext.current
+    //    val (showDialog, setShowDialog) = remember { mutableStateOf(show) }
+    //    ActionDialog(
+    //        title = context.getString(R.string.remove_widget_title), text = context.getString(R.string.remove_widget_text),
+    //        showDialog = showDialog, setShowDialog = setShowDialog,
+    //        confirmAction = { removeWidget(widget) },
+    //        dismissAction = {  },
+    //    )
 
     ScrollableColumn {
         widgets.forEach { widget ->
-            ActionDialog(
-                title = context.getString(R.string.remove_widget_title), text = context.getString(R.string.remove_widget_text),
-                showDialog = showDialog, setShowDialog = setShowDialog,
-                confirmAction = { onRemoveWidget(widget) },
-                dismissAction = { },
-            )
-
             Row(verticalAlignment = Alignment.CenterVertically) {
-                Column(Modifier
-                    .fillMaxWidth()
-                    .clickable(
-                        onClick = { },
-                        onLongClick = {
-                            setShowDialog(true)
-                        }), horizontalAlignment = Alignment.CenterHorizontally) {
-                    AndroidView(
-                        viewBlock = {
-                            createWidgetHostView(widget).apply {
-                                // layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-                                setOnLongClickListener {
-                                    setShowDialog(true)
-                                    true // <- set to true
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .clickable(
+                            onClick = { },
+                            onLongClick = {
+                                removeWidget(widget)
+                            }), horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Column(Modifier.padding(horizontal = 15.dp)) {
+                        AndroidView(
+                            viewBlock = {
+                                createWidgetHostView(widget).apply {
+                                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+                                    setOnLongClickListener {
+                                        removeWidget(widget)
+                                        true // <- set to true
+                                    }
                                 }
-                            }
-                        })
+                            })
+                    }
                 }
             }
         }
